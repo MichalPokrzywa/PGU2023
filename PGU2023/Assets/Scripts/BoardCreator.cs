cd197568c33fdb35f5fea216dc16e645339be9b8
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2023-10-20 15:06:00 +0200

    skrypt z zajęć

--- PGU2023/Assets/Scripts/BoardCreator.cs
+++ PGU2023/Assets/Scripts/BoardCreator.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class BoardCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject tilePrefab;
     [SerializeField] int n;
     [SerializeField] int m;
-    [SerializeField] float toleSize;
+    [SerializeField] float tileSize;
+    // Start is called before the first frame update
+    
     void Start()
     {
-        
+
+
+
+        for (int i = 0; i < n; i++)
+        {
+
+
+
+            for (int j = 0; j < m; j++)
+            {
+
+
+
+                Instantiate(tilePrefab, new Vector3(1.1f * i * tileSize, 0f, 1.1f * j * tileSize), Quaternion.identity, this.transform);
+            }
+        }
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
