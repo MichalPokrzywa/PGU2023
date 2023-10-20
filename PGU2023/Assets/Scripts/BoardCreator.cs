a848143e49307f4fe427f8285fa4d187f40b4a75
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2023-10-20 16:01:06 +0200

    garaż asset

--- PGU2023/Assets/Scripts/BoardCreator.cs
+++ PGU2023/Assets/Scripts/BoardCreator.cs
@@ -4,34 +4,29 @@ using UnityEngine;
 
 public class BoardCreator : MonoBehaviour
 {
-    [SerializeField] GameObject tilePrefab;
+  [SerializeField] GameObject tilePrefab;
     [SerializeField] int n;
     [SerializeField] int m;
-    float tileSize;
+    [SerializeField] float tileSize;
     // Start is called before the first frame update
-    
-    void Start()
-    {
-        tileSize = tilePrefab.transform.localScale.x * 10;
+    void Start() {
 
+ 
 
+        for(int i = 0; i < n; i++) {
 
-        for (int i = 0; i < n; i++)
-        {
+ 
 
+            for(int j = 0; j < m; j++) {
 
+ 
 
-            for (int j = 0; j < m; j++)
-            {
-
-
-
-                Instantiate(tilePrefab, new Vector3(1.1f * i * tileSize, 0f, 1.1f * j * tileSize), Quaternion.identity, this.transform);
+                Instantiate(tilePrefab,new Vector3(1.1f*i*tileSize , 0f,1.1f* j*tileSize), Quaternion.identity,this.transform);
             }
         }
     }
 
-
+ 
 
     // Update is called once per frame
     void Update()
