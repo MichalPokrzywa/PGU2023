810a5d792ee46540b8c067d68fdd7049130c0294
MichalPokrzywa <michpok467@student.polsl.pl>
2023-10-21 18:26:47 +0200

    Code refactor

---
+++ PGU2023/Assets/Scripts/Utility.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class Utility
+{
+    public static T[] GetAtPath<T>(string path)
+    {
+
+        ArrayList al = new ArrayList();
+        string[] fileEntries = Directory.GetFiles(Application.dataPath + "/" + path);
+        foreach (string fileName in fileEntries)
+        {
+            int index = fileName.LastIndexOf("/");
+            string localPath = "Assets/" + path;
+
+            if (index > 0)
+                localPath += fileName.Substring(index);
+
+            Object t = UnityEditor.AssetDatabase.LoadAssetAtPath(localPath, typeof(T));
+
+            if (t != null)
+                al.Add(t);
+        }
+        T[] result = new T[al.Count];
+        for (int i = 0; i < al.Count; i++)
+            result[i] = (T)al[i];
+
+        return result;
+    }
+}
