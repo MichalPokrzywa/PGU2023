cc741bdd90449e1ee14d5d06ff20c61c96b0be05
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2023-10-24 13:27:50 +0200

    karty poprawnie rozkładają się na scenie finalnej plus dodany limit

--- PGU2023/Assets/Scripts/HandManager.cs
+++ PGU2023/Assets/Scripts/HandManager.cs
@@ -7,9 +7,11 @@ public class HandManager : MonoBehaviour
     private static HandManager _instance;
     public static HandManager instance => _instance;
 
+    [SerializeField] private GameObject deck;
+
     public List<GameObject> cardObjects;
-    [SerializeField] float cardSize;
-    [SerializeField] int cardStart;
+    [SerializeField] float cardWidth;
+    [SerializeField] float maxNumberOfCarddsInHand;
     void Awake()
     {
         if (_instance != null && _instance != this)
@@ -24,27 +26,28 @@ public class HandManager : MonoBehaviour
     void Start()
     {
         cardObjects = new List<GameObject>();
-        for (int i = 0; i < cardStart; i++)
-        {
-            add(DeckManager.instance.getRandom());
-        }
     }
 
     public void add(GameObject card)
     {
-        Instantiate(card, new Vector3(1.2f * cardObjects.Count * cardSize, 0f, -13f), Quaternion.identity, this.transform);
-        cardObjects.Add(card);
+        if (!(cardObjects.Count == maxNumberOfCarddsInHand))
+        {
+            Debug.Log("y" + deck.transform.position.y);
+            Debug.Log("z" + deck.transform.position.z);
+            Debug.Log("x" + deck.transform.position.x);
+            Debug.Log(1.2f * cardObjects.Count * cardWidth + " " + deck.transform.position.y + " " + deck.transform.position.z);
+            Instantiate(card, new Vector3(deck.transform.position.x - (1.2f * (cardObjects.Count + 1) * cardWidth), deck.transform.position.y, deck.transform.position.z), Quaternion.identity, this.transform);
+            cardObjects.Add(card);
+        }
         //showCards();
     }
 
     void showCards()
     {
         //usunac stare
-        foreach(GameObject g in cardObjects)
+        foreach (GameObject g in cardObjects)
         {
             //g.Instantiate(tilePrefab, new Vector3(1.1f * i * tileSize, 0f, 1.1f * j * tileSize), Quaternion.identity, this.transform);
         }
-        
     }
-
 }
