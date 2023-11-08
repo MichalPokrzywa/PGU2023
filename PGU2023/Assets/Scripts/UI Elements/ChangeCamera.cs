ff4d461c9e76ecc8d0e5c41f5f4ab6cfe403914e
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2023-11-08 23:50:41 +0100

    bomb doesn't spin me right 'round, baby, right 'round

--- PGU2023/Assets/Scripts/UI Elements/ChangeCamera.cs
+++ PGU2023/Assets/Scripts/UI Elements/ChangeCamera.cs
@@ -7,12 +7,14 @@ public class ChangeCamera : MonoBehaviour
 
     public GameObject camera1;
     public GameObject camera2;
+    public GameObject bomb;
 
     void Start()
     {
         // Disable one of the cameras initially
         camera1.SetActive(true);
         camera2.SetActive(false);
+        bomb.SetActive(true);
     }
 
     // Call this method to swap the active cameras
@@ -21,5 +23,6 @@ public class ChangeCamera : MonoBehaviour
         GameManager.instance.isInWalkMode = !GameManager.instance.isInWalkMode;
         camera1.SetActive(!camera1.activeSelf);
         camera2.SetActive(!camera2.activeSelf);
+        bomb.SetActive(!bomb.activeSelf);
     }
 }
