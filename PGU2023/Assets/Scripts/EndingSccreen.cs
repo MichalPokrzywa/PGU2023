d8ba444361961d1d659f4731950b6d7d08eefdd1
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2023-11-09 13:46:32 +0100

    ending screen

---
+++ PGU2023/Assets/Scripts/EndingSccreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingSccreen : MonoBehaviour
+{
+    [SerializeField] GameObject cost;
+    [SerializeField] GameObject value;
+    [SerializeField] GameObject intrest;
+    [SerializeField] GameObject vunctionality;
+    [SerializeField] GameObject nickname;
+    [SerializeField] GameObject diffImage;
+    [SerializeField] GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
